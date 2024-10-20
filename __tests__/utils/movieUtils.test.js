ecc3e0e8ab4fd49321ff270b62eaacbdc6b5719b
Angela Flynn <156027242+Ang3la33@users.noreply.github.com>
2024-10-20 14:36:28 -0230

    Update movieUtils.test.js
    Testing testing

--- __tests__/utils/movieUtils.test.js
+++ __tests__/utils/movieUtils.test.js
@@ -1,5 +1,5 @@
 const { getRandomMoviesByGenre, getTopRatedMovies, formatMovieData, getRandomGenre, generateMovieReport } = require("../../utils/movieUtils");
-
+//testing af
 describe('Movie Utility Functions', () => {
     describe('getMoviesByGenre', () => {
 
